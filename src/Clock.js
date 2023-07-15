6cbca260fe78ed36a58ee421dd473c84f863ea03
Antonina Davydova <antoninadd22@gmail.com>
2023-07-14 21:33:11 -0600

    added files

---
+++ src/Clock.js
@@ -0,0 +1,32 @@
+import { Component } from "react";
+
+export default class Clock extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            time: new Date().toLocaleTimeString()
+        }
+    }
+    
+    componentDidMount(){
+        this.timerID = setInterval(()=>
+        this.tick(), 1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerID)
+    }
+
+
+
+    tick(){
+        this.setState({
+            time: new Date().toLocaleTimeString()
+        })
+    }
+
+    render(){
+        return <p className="Clock">{this.state.time}</p>
+    }
+}
